7961f42e2e1b4bc26ad9b5e1dd9e0e02a6a3b00a
hovu2001 <nguyenduyhoang26042002@gmail.com>
2025-07-10 19:13:55 +0700

    video17

---
+++ src/main/java/com/laptop_shop/HelloController.java
@@ -0,0 +1,13 @@
+package com.laptop_shop;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/")
+    public String index() {
+        return "Hello World from Spring Boot!";
+    }
+}
