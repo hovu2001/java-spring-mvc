314f854bdc64a835671bc3befd34a21588667dfb
hovu2001 <nguyenduyhoang26042002@gmail.com>
2025-07-13 20:43:05 +0700

    video45

---
+++ src/main/java/com/laptop_shop/service/UserService.java
@@ -0,0 +1,11 @@
+package com.laptop_shop.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+    public String handelHello(){
+        return "Hello from service";
+    }
+
+}
